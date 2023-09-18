273f9aa7018ab21a08f0f328f152709b465fbe7b
Sandro Generoso <106679349+sandrogeneroso@users.noreply.github.com>
2023-09-18 10:41:10 -0300

    aula 18/09

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom () {
-    document.querySelector ('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`;//template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    //console.log(contador);
+
+}
